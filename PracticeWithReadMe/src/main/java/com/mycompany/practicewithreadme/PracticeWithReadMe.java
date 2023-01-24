a72bc4b852df653b477507dfb64286ef8c7baaf6
deleo <deleo@Home-PC>
2023-01-23 20:54:42 -0600

    Added another print line

---
+++ PracticeWithReadMe/src/main/java/com/mycompany/practicewithreadme/PracticeWithReadMe.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicewithreadme;
+
+/**
+ *
+ * @author deleo
+ */
+public class PracticeWithReadMe {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
