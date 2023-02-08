73650390a1cd5ff4fb8184925901710cb11b5922
deleo <deleo@Home-PC>
2023-02-07 22:39:44 -0600

    Added README docs, finalized Gerald's story, and formatted text

--- GUIInformationalDisplay/src/guiinformationaldisplay/GUIInformationalDisplay.java
+++ GUIInformationalDisplay/src/guiinformationaldisplay/GUIInformationalDisplay.java
@@ -1,14 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
- */
 package guiinformationaldisplay;
 
 import javafx.application.Application;
 import javafx.geometry.HPos;
 import javafx.scene.Scene;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
@@ -18,14 +12,14 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.control.Button;
 import javafx.scene.text.Font;
-import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-
+import javafx.scene.text.TextAlignment;
 
 /**
- *
- * @author cdeleon
+ * Chris de Leon
+ * 2/7/2023
+ * GUI Informational Display
  */
 public class GUIInformationalDisplay extends Application {
     
@@ -44,36 +38,46 @@ public class GUIInformationalDisplay extends Application {
         Image image = new Image("image/SadDog.jpg");
         // add image to window
         
-        // heading text 
-        Label heading = new Label("HEADING TEXT");
-        heading.setFont(Font.font("Times New Roman", FontWeight.BOLD, 20));
+        // heading text and styling
+        Label heading = new Label("Adopt This Dog");
+        heading.setFont(Font.font("Times New Roman", FontWeight.BOLD, 24));
         pane.add(heading, 0, 0);
-        // heading styling
+
+        // subheading text and styling
+        Label subHeading = new Label("Gerald");
+        subHeading.setFont(Font.font("Times New Roman", FontWeight.BOLD, 20));
+        pane.add(subHeading, 0, 1);
         
-        // image
-        pane.add(new ImageView("image/SadDog.jpg"), 0, 1);
+        // centering for heading and subheading
+        GridPane.setHalignment(heading, HPos.CENTER);
+        GridPane.setHalignment(subHeading, HPos.CENTER);
+        
+        // image add and positioning
+        ImageView SadDog = new ImageView("image/SadDog.jpg");
+        pane.add(SadDog, 0, 2);
+        GridPane.setHalignment(SadDog, HPos.CENTER);
+        
         // text box
-        pane.add(new Text("TEXT BOX"), 0, 2);
+        Text body = new Text("Gerald is one of the kindest dogs that is just down on his luck." 
+        + "\nLike the majority of dogs, Gerald was once a part of an extremely loving family."
+        + "\nUnfortunately for him though, his entire family passed in a horrific boating accident. He was the only survivor"
+        + "\nHe went from being the happiest soul on earth, to the saddest dog known to man."
+        + "\nAt Dog Fostering USA, we believe that you are the key to Gerald's joy and we'd love to bring you two together."
+        + "\nPlease click the button at the bottom of Gerald's adopt-me page to give this pup the life he deserves.");
+        body.setStyle("-fx-font: 16 'Times New Roman';");
+        pane.add(body, 0, 3);
+        body.setTextAlignment(TextAlignment.CENTER); // centers all body text
         
         // button
         Button adoptButton = new Button("Adopt Me!");
-        pane.add(adoptButton, 0, 3);
+        pane.add(adoptButton, 0, 4);
         GridPane.setHalignment(adoptButton, HPos.CENTER);
-        
-        
-        
-        
         
         // create scene object, store the pane, and render to the screen using primary stage
         Scene scene = new Scene(pane);
-        primaryStage.setTitle("Dog Fostering - Making a Difference, One Dog at a Time. ");
+        primaryStage.setTitle("Dog Fostering USA - Making a Difference, One Dog at a Time. ");
         primaryStage.setScene(scene);
         primaryStage.show();
-        
-       
-        
-        
-        
     }
 
     /**
@@ -82,5 +86,4 @@ public class GUIInformationalDisplay extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
 }
