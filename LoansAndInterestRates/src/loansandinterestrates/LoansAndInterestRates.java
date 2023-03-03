5d3203595d802122acbd08a54ce534effe684f24
Chris de Leon <95385991+chrisdeleon@users.noreply.github.com>
2023-03-03 16:50:00 -0600

    Adjusted methods, added try-catch, and formatting
    Formatting still needs some adjustments. The numbers are displaying correctly, but they are missing two decimal places. After that, the project is nearly finished, excluding the README.

--- LoansAndInterestRates/src/loansandinterestrates/LoansAndInterestRates.java
+++ LoansAndInterestRates/src/loansandinterestrates/LoansAndInterestRates.java
@@ -2,12 +2,13 @@ package loansandinterestrates;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import java.util.Scanner;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
 /**
  * Chris de Leon
@@ -24,49 +25,89 @@ payments for each interest rate starting from 5% to 8%, with increments of one-e
 and displays the monthly total payments for each interest... */
 public class LoansAndInterestRates extends Application {
     
+    Loan FirstLoan = new Loan();
     @Override
     public void start(Stage primaryStage) {
+        
+        // creates a grid pane and adds spacing
+        GridPane pane = new GridPane();
+        pane.setHgap(5.5); 
+        pane.setVgap(5.5); 
+        
+        // create text area for calculation output
+        TextArea calculationDisplay = new TextArea();
+        calculationDisplay.setText("Interest   Monthly Payment    Total Payment    \n");
+
+        
+        // text fields to grab data for calculation
+        TextField amount = new TextField();
+        TextField years = new TextField();
+        
+        // button that triggers calculation method
         Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+        btn.setText("Show Table");
+        btn.setOnAction((ActionEvent event) -> {
+            // runs the calculation method, but changes the entered numbers to the required variable type first
+            // try-catch added for text entered that cannot be changed to double or integer
+            try {
+                // for loop calculate loan rates from 5% to 8%
+                for(double i = 5; i <= 8; i = i + 0.125){
+                FirstLoan.LoanCalculate(Double.parseDouble(amount.getText()), 
+                        Integer.parseInt(years.getText()), i);
+                calculationDisplay.appendText(FirstLoan.getData() + "\n");
+                }
+            } catch(NumberFormatException e){
+                System.out.println("Please enter whole numbers");
             }
         });
-        Loan.LoanCalculate(250000, 15);
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
         
-        Scene scene = new Scene(root, 300, 250);
         
-        primaryStage.setTitle("Hello World!");
+        // adds different elements to the grid
+        pane.add(new Label("Loan Amount:"), 0, 0);
+        pane.add(amount, 1, 0);
+        pane.add(new Label("Number of Years:"), 2, 0); 
+        pane.add(years, 3, 0);
+        pane.add(btn, 4, 0);
+        pane.add(calculationDisplay, 0, 1, 4, 4);
+        
+        
+        // adds grid to the scene, sets the stage, and displays it
+        Scene scene = new Scene(pane, 650, 400);
+        primaryStage.setTitle("Loan Calculator");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
     
     // Loan class, where all loan calculations will take place
     class Loan {
+        private int monthlyPayment = 0;
+        private int totalPayment = 0;
+        private double interestRate = 0;
     
     // method for calculating loan terms
-    public static void LoanCalculate(double amount, int years){
-        // for loop calculate loan rates from 5% to 8%
-        for(double i = 5; i <= 8; i = i + 0.125){
+    public void LoanCalculate(double amount, int years, double interestRate){
             // obtains monthly interest rate
-            double monthlyInterestRate = i / 1200;
+            double monthlyInterestRate = interestRate / 1200;
             // math for calculating monthly rate, please note this code is "legacy" code
             double monthlyPayment = amount * monthlyInterestRate /(1 - 1 / Math.pow(1 + monthlyInterestRate, years * 12));
+            // coverts the monthly payment to integer
+            this.monthlyPayment = (int) ((monthlyPayment * 100) / 100.0);
             // total amount paid, including interest
             double totalPayment = monthlyPayment * years * 12;
+            // converts the total payment to integer
+            this.totalPayment = (int)((totalPayment * 100) / 100.0);
+            // sets interest rate to current loop iteration
+            this.interestRate = interestRate;
             
-            // formats the variables into readable numbers for the user
-            System.out.print("Monthly payment: " + (int)(monthlyPayment * 100) / 100.0);
-            System.out.print(" Total payment: " + (int)(totalPayment * 100) / 100.0 + " at " + i + "% interest rate");
-            System.out.println("");
-        }
+            // getter methods update the private variable each iteration of the loop, which allows the correct numbers to display
+            
     }
+    
+    // returns string with data
+    public String getData(){
+        return String.format( "%2s %-22s %25s", this.interestRate, this.monthlyPayment, this.totalPayment  );
+    }
+    
 }
 
     /**
